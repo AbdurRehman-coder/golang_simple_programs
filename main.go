9e95136bcc7ba9de94adf336f3c5abf74ebf64e0
AbdurRehman-coder <abdurrehmanlkl786@gmail.com>
2023-01-25 00:01:28 +0500

    declaration with initialization

--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	// my first line of code in go, just printing
 	fmt.Println("My first go programm")
-	// adding two numbers
-	a := 34
-	var b = 9
+
+	/// Declaration with initialization
+	const a int8 = 34
+	var b int8 = 9
 	var result = a + b
-	fmt.Printf("the result is: %p\n", result)
+	// formatted print
+	fmt.Printf("the result is: %d\n", result)
+	// simple print
+	fmt.Println("the result is: ", result)
+
+	// var name = "Abdur Rehman"
+	// userName := "Shinwari"
+
+	// fmt.Println("My name is:", name, userName)
+
+	var fname, lname = "Arshad", "Ali"
+	fmt.Println("first & last name: ", fname, lname)
 
 }
